54c8b0ac698e88f860455d41b34a26344073a64e
SaiRaghavArramSema4 <sairaghav.arram@sema4genomics.com>
2020-06-30 11:53:11 +0530

    Initial commit

---
+++ src/main/java/com/pramati/calculatorservice/CalcService.java
@@ -0,0 +1,21 @@
+package com.pramati.calculatorservice;
+
+public class CalcService {
+	
+	public static int add(int a, int b) {
+		return a+b;
+	}
+	
+	public static int sub(int a, int b) {
+		return a-b;
+	}
+	
+	public static int mulitply(int a, int b) {
+		return a*b;
+	}
+	
+	public static int divide(int a, int b) {
+		return a/b;
+	}
+
+}
